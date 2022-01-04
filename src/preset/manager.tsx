be7849f0dd76cc7d5019df1ba3c74bb4765e3046
Steven Sacks <stevensacks@gmail.com>
2022-01-04 17:25:38 +0900

    feat(tool): show on Docs panel

--- src/preset/manager.tsx
+++ src/preset/manager.tsx
@@ -7,7 +7,7 @@ addons.register(ADDON_ID, () => {
     addons.add(TOOL_ID, {
         title: 'Storybook i18n',
         type: types.TOOL,
-        match: ({viewMode}) => viewMode === 'story',
+        match: ({viewMode}) => !!viewMode?.match(/^(story|docs)$/),
         render: () => <Tool />,
     });
 });
