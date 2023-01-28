ebcf7c1f906835038343dd7353a57768e2ddac10
陈阳 <chenyang@shimo.im>
2023-01-28 17:08:18 +0800

    Add locale to globals

--- src/constants.ts
+++ src/constants.ts
@@ -1,2 +1,3 @@
 export const ADDON_ID = 'storybook/i18n-addon';
 export const TOOL_ID = 'storybook/i18n-addon/tool';
+export const PARAM_KEY = 'locale';
--- src/preset/preview.ts
+++ src/preset/preview.ts
@@ -1,3 +1,7 @@
 import {withLocale} from '../withLocale';
+import { PARAM_KEY } from '../constants';
 
 export const decorators = [withLocale];
+export const globals = {
+  [PARAM_KEY] : false
+};
