f20f58e17caaaf5905ba49000f692aea5260f91e
Steven Sacks <stevensacks@gmail.com>
2023-04-07 18:10:55 +0900

    add satisfies to preview

--- src/preview.ts
+++ src/preview.ts
@@ -1,12 +1,12 @@
 import type {Renderer, ProjectAnnotations} from '@storybook/types';
 import {withLocale} from './withLocale';
 
-const preview: ProjectAnnotations<Renderer> = {
+const preview = {
     decorators: [withLocale],
     globals: {
         locale: '',
         locales: {},
     },
-};
+} satisfies ProjectAnnotations<Renderer>;
 
 export default preview;
