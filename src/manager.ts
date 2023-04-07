71dda7a0b344c93778f398659a12ea46dc2fa574
Steven Sacks <stevensacks@gmail.com>
2023-04-07 14:01:56 +0900

    feat: migrate to Storybook 7

--- src/manager.ts
+++ src/manager.ts
@@ -1,13 +1,12 @@
-import React from 'react';
-import {addons, types} from '@storybook/addons';
-import {ADDON_ID, TOOL_ID} from '../constants';
-import Tool from '../Tool';
+import {addons, types} from '@storybook/manager-api';
+import {ADDON_ID, TOOL_ID} from './constants';
+import Tool from './Tool';
 
 addons.register(ADDON_ID, () => {
     addons.add(TOOL_ID, {
         title: 'Storybook i18n',
         type: types.TOOL,
         match: ({viewMode}) => !!viewMode?.match(/^(story|docs)$/),
-        render: () => <Tool />,
+        render: Tool,
     });
 });
