ce561dda1b8c65e0a50cd2781b519a857436b166
Steven Sacks <stevensacks@gmail.com>
2025-11-06 14:11:16 +0900

    chore: remove unused param

--- src/manager.tsx
+++ src/manager.tsx
@@ -3,7 +3,7 @@ import {addons, types} from 'storybook/manager-api';
 import {ADDON_ID, TOOL_ID} from './constants';
 import Tool from './Tool';
 
-addons.register(ADDON_ID, (api) => {
+addons.register(ADDON_ID, () => {
     addons.add(TOOL_ID, {
         title: 'Storybook i18n',
         type: types.TOOL,
